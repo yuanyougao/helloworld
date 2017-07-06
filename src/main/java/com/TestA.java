9ecd5ad97d51306dbba3b6398320958be82fac92
yuanyougao <yuanyougao@163.com>
2017-07-06 20:52:40 +0800

    提交代码

--- src/main/java/com/TestA.java
+++ src/main/java/com/TestA.java
@@ -5,7 +5,7 @@ public class TestA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("liuju");
+		    System.out.println("liuju");
 		
 		System.out.println("liujua");
 	}
